edfe93eb7eeaea6b6eed889d6d82abc20497cd1c
anujgangrade <121935500+anujgangrade48@users.noreply.github.com>
2024-02-27 19:40:34 +0000

    convertion of type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score =  NaN
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+// console.log(typeof valueInNumber);  // number
+
+// "33" => 33
+// "33abs" => NaN
+// null => 0
+// true => 1; false => 0
+// undefined => NaN  
+// "anuj" => NaN
+
+let isLoggesIn = "anuj"
+
+let boleaonIsLoggedIn = Boolean(isLoggesIn)
+
+console.log(boleaonIsLoggedIn);
+
+// 1=> true; 0=> false
+//""=> false; "anuj"=> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// 33 => "33"  //convert string
