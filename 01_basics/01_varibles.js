056ee524b35b2ec03da79d28c7626256038f5a9a
anujgangrade <121935500+anujgangrade48@users.noreply.github.com>
2024-02-27 18:51:40 +0000

    varibles

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer version (esse new version ko treat krne ke liye kiya jata hai )
+
+//alert(3+3) // we are using browser not use NodeJs && this code Error ReferenceError: alert is not defined
+
+let name = "Anuj" //string type
+let age = 22 // number type
+let isLoggesIn = false  // boleaon
+let state = null
+let city
+
+//premitive data type---
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undifind =>
+// symbol => unique 
+
+// Object
+
+console.log(typeof state); // object
+console.log(typeof city); // undefined
+
+
