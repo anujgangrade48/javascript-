099b6f9a582b947c8939b87ae9a52d257f19c138
anujgangrade <121935500+anujgangrade48@users.noreply.github.com>
2024-02-28 19:16:28 +0000

    summary of data type

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,47 @@
+// two categorys hoti hai 
+
+// 1.primitive 
+// #def = yaha call by value hote hai jb bhi kahi se copy krte hai jo orignial data hai usemse copy krke de hai 
+// AND jo bhi changes hote hai wo copy me hote hai
+
+// 7 types : String, Number, Boolean, null, undefined, bigint, symbol
+
+// const stringValue = "Anuj";
+// const numberValue = 100
+// const number = 100.3
+// const booleanValue = false
+// const nullValue = null // 
+// let myName;  // undefiend
+// let bigNumber = 465464546464465n // n lagane se bigInt ho jati hai value
+// let id = Symbol('123') //unique value pass krta hai
+// let idName = Symbol('123')
+
+
+// console.log(idName == id);
+
+
+
+// Reference type (non primitive)
+
+// Array, Objects, Functions
+
+// const heros = ["saktiman","ram babu", "rajpal yadav"] // this is array  but type of Object hai 
+
+// const myobj = {
+//     name: "anuj",
+//     age: 22,
+// } // this is object type of Object hai
+
+
+//function 
+
+// function myFunction(){
+//     console.log('hello');
+// } // eska type function hota hai but ese js me object hi kahate hai 
+
+// const myFunction = function(){
+// }
+// console.log(typeof myFunction);
+
+
+// *******************ecma script pe hai *********************
