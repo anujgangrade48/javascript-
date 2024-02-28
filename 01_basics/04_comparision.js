eb13434298109972bac1aaeaa16228bc8dda9f93
anujgangrade <121935500+anujgangrade48@users.noreply.github.com>
2024-02-28 19:15:35 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// comparision
+
+// console.log(2 > 1);  true
+// console.log(2 >= 1); true
+// console.log(2 == 1); false
+// console.log("1" == 1);  true 
+
+
+
+// console.log(null > 0);  false 
+// console.log(null >= 0); true
+// console.log();
+
+
+
+// comparition and equlity check alag alag hai jo  == and === alag work krta hai  AND < > ye alag work krta hai 
+
+//  === value ke sath dataType bhi check krta hai 
+
+console.log("1" === 1); // false 
+console.log(1 === 1); //true
